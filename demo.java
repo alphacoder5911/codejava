6ac71fe0d7b7069aa29ce24d962877735d7d113f
alphacoder5911 <lalgeshreyas@gmail.com>
2023-12-22 12:54:55 +0530

    JUST CHILL

--- demo.java
+++ demo.java
@@ -1,5 +1,20 @@
+
+import java.util.Scanner;
 class demo{
     public static void main(String[] args) {
         System.out.println("HI SHREYAS");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE AGE OF SHREYAS ");
+        int s=sc.nextInt();
+        System.out.println("THE AGE IS "+s);
+        if(s>18)
+        {
+            System.out.println("SHREYAS CAN HAVE A DRIVERS LICSENCE");
+        }
+        else
+        {
+            System.out.println("LICENCE NAHI MILEGA");
+        }
+        
     }
 }
